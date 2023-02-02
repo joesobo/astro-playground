79c1308123d01bb0e1dc0e8f5c964665ac2bf883
joesobo <soboleskijoe@gmail.com>
2023-02-01 17:10:23 -0800

    install vue island

---
+++ src/components/Greetings.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>{{ greeting }}! Thank you for visiting!</h3>
+    <button @click="setGreeting(randomMessage())">New Greeting</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{ messages: string[] }>();
+
+const randomMessage = () => {
+  const randomIndex = Math.floor(Math.random() * props.messages.length);
+  return props.messages[randomIndex];
+};
+
+const greeting = ref(randomMessage());
+
+const setGreeting = (newGreeting: string) => {
+  greeting.value = newGreeting;
+};
+</script>
